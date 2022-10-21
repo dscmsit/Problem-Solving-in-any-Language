4a861420e0222d95a87b63b387dc2cc58c86e1b7
engineer-aslam-hossain <engineer.aslamhossain@gmail.com>
2022-10-21 12:47:47 +0600

    engineer-aslam-hossain prime number in javascript

--- prime-number.js
+++ prime-number.js
@@ -1,5 +1,3 @@
-// program to check if a number is prime or not
-
 // take input from the user
 const number = parseInt(prompt("Enter a positive number: "));
 let isPrime = true;
@@ -7,10 +5,7 @@ let isPrime = true;
 // check if number is equal to 1
 if (number === 1) {
   console.log("1 is neither prime nor composite number.");
-}
-
-// check if number is greater than 1
-else if (number > 1) {
+} else if (number > 1) {
   // looping through 2 to number-1
   for (let i = 2; i < number; i++) {
     if (number % i == 0) {
@@ -24,9 +19,6 @@ else if (number > 1) {
   } else {
     console.log(`${number} is a not prime number`);
   }
-}
-
-// check if number is less than 1
-else {
+} else {
   console.log("The number is not a prime number.");
 }
