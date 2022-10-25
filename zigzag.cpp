0de244866235f485005753f71f66c9c33fca5eda
Himanshi1805 <82957564+Himanshi1805@users.noreply.github.com>
2022-10-25 18:46:36 +0530

    Added ZigzagConversions.cpp

---
+++ zigzag.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define rep(int, i, z) for (int i = 0; i < z; i++)
+class Solution {
+public:
+string convert(string s, int numRows)
+{
+
+    if (numRows == 1)
+        return s;
+
+    vector<string> rows(min(numRows, int(s.size())));
+    int curRow = 0;
+    bool goingDown = false;
+
+    for (char c : s)
+    {
+        rows[curRow] += c;
+        if (curRow == 0 || curRow == numRows - 1)
+            goingDown = !goingDown;
+        curRow += goingDown ? 1 : -1;
+    }
+
+    string ret;
+    for (string row : rows)
+        ret += row;
+    return ret;
+}
+};
