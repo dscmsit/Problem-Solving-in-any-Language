2831642aa74b599d61b1d344f02889cea006e037
Ekraj Ghimire <117092834+ekrajghimire127@users.noreply.github.com>
2022-10-31 22:37:16 +0530

    Add files via upload

---
+++ InheritConst.java
@@ -0,0 +1,34 @@
+//package inheritconst;
+public class InheritConst 
+{
+
+    public static void main(String[] args) 
+    {
+        GrandChild c=new GrandChild();        
+    }
+    
+}
+class Parent
+{
+    public Parent()
+    {
+        System.out.println("Parent Constrcutor");
+    }
+            
+}
+
+class Child extends Parent
+{
+    public Child()
+    {
+        System.out.println("Child Constructor");
+    }
+}
+
+class GrandChild extends Child
+{
+    public GrandChild()
+    {
+        System.out.println("Grand Child Constructor");
+    }
+}
